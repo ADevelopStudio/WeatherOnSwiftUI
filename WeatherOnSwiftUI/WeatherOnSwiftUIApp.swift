80681ebe917fad393881f707d2945c1640fb2ab4
Dmitrii Zverev <dima.newmail@gmail.com>
2022-08-22 18:16:08 +1000

    Initial Commit

---
+++ WeatherOnSwiftUI/WeatherOnSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherOnSwiftUIApp.swift
+//  WeatherOnSwiftUI
+//
+//  Created by Dmitrii Zverev on 22/8/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherOnSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
